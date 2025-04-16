2c1c9bd7e46393646544fe5902c246d61fac0080
Dave <dave@dotloadmovie.com>
2025-04-16 11:30:21 +0100

    [feat]: conditional splash

--- src/atoms/user.ts
+++ src/atoms/user.ts
@@ -1,6 +1,6 @@
 import {atomWithStorage, createJSONStorage} from 'jotai/utils';
 
 const storage = createJSONStorage(() => sessionStorage);
-const username = atomWithStorage('username', 'User', storage);
+const username = atomWithStorage('username', '', storage);
 
 export {username}
