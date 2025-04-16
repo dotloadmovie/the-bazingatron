3cce411a7bfec0f9509d68880165a143f1554604
Dave <dave@dotloadmovie.com>
2025-04-16 06:45:19 +0100

    [feat]: initial commit, clean up files and set to base

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,18 +1,9 @@
-import automataLogo from '../assets/automata.png'
-import './App.css'
-
 function App() {
   return (
     <>
-      <div>
-        <a href="https://automata.tech/" target="_blank">
-          <img src={String(automataLogo)} className="logo automata" alt="Automata logo"/>
-        </a>
-      </div>
-      <h1>Frontend Exercise</h1>
-      <h2>Rock, Paper, Scissors, Lizard, Spock</h2>
+      <div>Base</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
